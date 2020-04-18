02a6903fab9a4871cbce3369d9ccad01221bf491
StacyLeith <63213437+StacyLeith@users.noreply.github.com>
2020-04-18 09:33:09 +0100

    Added 2nd bot token

--- config/botcfg.py
+++ config/botcfg.py
@@ -1,5 +1,6 @@
 # CONFIG VARIABLES
 
-TOKEN = "NDQzMzEyMzY0MTIxNjIwNDgw.DdLiww.nISChyjinfVj4nd8Bu3qrJ_0LZ8"
+#TOKEN = "NDQzMzEyMzY0MTIxNjIwNDgw.DdLiww.nISChyjinfVj4nd8Bu3qrJ_0LZ8"  # Nimabot
+TOKEN = "NzAwOTc4MjMyMDMxMzEzOTcx.Xpq7EA.QyUku53nRWRbz7eEREBedCzwNzE"  # Deltabot
 GAME = "Half Life 4"
 PREFIX = "!"
